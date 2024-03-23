35a86558dc49d80f84ba88ed2571e58a7d1f1b1e
Jamalshah <jimisk811@gmail.com>
2024-03-23 18:19:12 +0500

    update user route, contoller and varifyuser created

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { test } from "../controllers/user.contoller.js";
+import { test, updateUser } from "../controllers/user.contoller.js";
+import { varifyToken } from "../utils/varifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.put("/update/:userId", varifyToken, updateUser);
 export default router;
